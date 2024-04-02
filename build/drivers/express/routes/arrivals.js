b7bd7aa75111102ce23326f9281cbf12ad30cae4
Nazar Korpal <nko@tricent.com>
2024-04-03 02:46:59 +0300

    deleted build files

--- build/drivers/express/routes/arrivals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function arrivalRoutes(app, controller) {
-    app.use("/arrivals");
-    app.post("", controller.addArrival);
-    return app;
-}
-exports.default = arrivalRoutes;
--- build/drivers/express/routes/products.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-function productRoutes(controller) {
-    const router = (0, express_1.Router)();
-    router.post("", controller.addProduct);
-    return router;
-}
-exports.default = productRoutes;
